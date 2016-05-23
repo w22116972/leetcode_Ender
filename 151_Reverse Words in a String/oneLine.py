b937c82683c51138f8ab22fe2b1421dd0cf88d7a
Ender Wang <w22116972@gmail.com>
2016-05-23 20:25:04 +0800

    add comment

--- 151_Reverse Words in a String/oneLine.py
+++ 151_Reverse Words in a String/oneLine.py
@@ -4,4 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # strip() can be removed since split() will handle beginning and ending spaces
+        # ' '.join(split()) can remove duplicated spaces
         return ' '.join(reversed(s.split()))
